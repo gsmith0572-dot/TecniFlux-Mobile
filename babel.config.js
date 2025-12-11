e5c5239641302007cf2c4385dbe61c80e9ce106d
George Smith <mbp_alfa_01@MBPALFA01.local>
2025-12-11 16:52:40 -0600

    Remove: NativeWind de babel.config.js - dejar solo babel-preset-expo y react-native-reanimated/plugin

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-    ],
-    plugins: [
-      'nativewind/babel',
-      "react-native-reanimated/plugin" // IMPORTANTE: Debe ser el último
-    ],
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'], // IMPORTANTE: Debe ser el último
   };
 };
