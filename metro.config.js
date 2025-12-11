d7e1b88fc4d2c92dd4dadc75c36c1ea926960e1e
George Smith <mbp_alfa_01@MBPALFA01.local>
2025-12-11 17:10:00 -0600

    fix: remove NativeWind from metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Configuración para production: resetCache en true
-const isProduction = process.env.NODE_ENV === 'production';
-if (isProduction) {
-  config.resetCache = true;
-}
-
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = config;
